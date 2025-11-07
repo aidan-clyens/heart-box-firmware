08af31b1c8fca5f91b79a052ca09709652c52d56
Aidan Clyens <aidanc@rogers.com>
2025-11-07 12:50:28 -0500

    Initialize State Machine Task in test app

--- test_app/main/test_app_main.c
+++ test_app/main/test_app_main.c
@@ -1,30 +1,35 @@
 #include "unity.h"
 #include "unity_fixture.h"
 
+#include "file_system.h"
+#include "state_machine_task.h"
+
 // Test group setup
-TEST_GROUP(testable);
-TEST_SETUP(testable)
+TEST_GROUP(state_machine_task);
+TEST_SETUP(state_machine_task)
 {
+  file_system_init();
+  state_machine_task_init();
 }
 
-TEST_TEAR_DOWN(testable)
+TEST_TEAR_DOWN(state_machine_task)
 {
 }
 
 // Testcases
-TEST(testable, example)
+TEST(state_machine_task, example)
 {
   TEST_ASSERT_EQUAL(1, 1);
 }
 
-TEST_GROUP_RUNNER(testable)
+TEST_GROUP_RUNNER(state_machine_task)
 {
-  RUN_TEST_CASE(testable, example);
+  RUN_TEST_CASE(state_machine_task, example);
 }
 
 static void run_all_tests(void)
 {
-  RUN_TEST_GROUP(testable);
+  RUN_TEST_GROUP(state_machine_task);
 }
 
 // Test application main

3eb2e1c39ebf6ada651ab29c06a1237d0ad0d1ab
Aidan Clyens <aidanclyens@gmail.com>
2026-07-01 13:25:10 -0400

    Update git submodules in Docker entrypoint.sh script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "source /root/.espressif/tools/activate_idf_v5.4.2.sh" >> ~/.bashrc
 git config --global --add safe.directory $(pwd)
+git submodule sync --recursive
+git -c protocol.version=2 submodule update --init --force --depth=1 --recursive
 exec "$@"
